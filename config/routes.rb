6d08e8b272ddacc9245b16cad3595aa74892b8d6
スタディプレイス.me <studyplace.me_19@sutadipureisumenoMacBook-Air.local>
2021-04-17 16:37:20 +0900

    [add]パスワードリセット機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get    '/help',    to: 'static_pages#help'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
@@ -12,4 +14,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
